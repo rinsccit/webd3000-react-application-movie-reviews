98c3a0786f5d20742f6457714754ee1828cc9052
Rahul Sood <w0510619@campus.nscc.ca>
2026-04-01 23:40:12 -0300

    Started working on the React frontend website

---
+++ react-application-movie-reviews-webd3000/src/types/Movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: string,
+    title: string,
+    image: string,
+    synopsis: string,
+    runtime: number,
+    releaseDate: string,
+    createdBy: string,
+    
+}
